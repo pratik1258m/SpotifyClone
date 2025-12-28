867f2c84e09ab6c0e2c2ec044b25db3e765f10bd
pratik1258m <pratikmishra206@gmail.com>
2025-12-28 14:45:46 +0530

    update

--- script.js
+++ script.js
@@ -123,3 +123,4 @@ document.getElementById('previous').addEventListener('click', () => {
     updateSongListPlayStatus(songIndex, true);
     document.querySelector(".songInfo img").src = songs[songIndex].coverPath;
 })
+
